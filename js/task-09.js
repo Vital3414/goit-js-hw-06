cc448b91b01edc96db1629f122e5ba33f9c2994d
Vital3414 <qwer1234432148@gmail.com>
2023-03-27 10:10:09 +0300

    upd task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,19 +1,16 @@
-const widget = document.querySelector('.widget');
-const button = widget.querySelector('.change-color');
-const colorSpan = widget.querySelector('.color');
+const currentColorRef = document.querySelector('.color');
+
+const changeColorBtnRef = document.querySelector(".change-color");
+
+changeColorBtnRef.addEventListener("click", onChangeColorBtnClick);
+
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
-button.addEventListener('click', () => {
+function onChangeColorBtnClick(event) {
   const newColor = getRandomHexColor();
   document.body.style.backgroundColor = newColor;
-  colorSpan.textContent = newColor;
-});
+  currentColorRef.textContent = newColor;
+};
